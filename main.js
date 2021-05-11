4de2d62f142595c50738bd3d741664943b51cf6f
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-05-11 16:08:39 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//1
+let exo1 = document.getElementsByClassName("tartine");
+console.log(exo1);
+
+//2
+let exo2 = document.getElementById('toast');
+console.log(exo2);
+
+//3
+let exo3 = exo1[1].innerHTML;
+console.log(exo3);
